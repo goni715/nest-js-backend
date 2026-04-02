9c01f9d9d74a8e2d75e55f2eed9e9234420f0ecd
goni715 <halesgoni715149123@gmail.com>
2026-04-02 22:53:27 +0600

    update user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.schema';
 
@@ -10,5 +10,27 @@ export class UserController {
   async addUser(@Body() data: Partial<User>) {
     console.log(data);
     return await this.userService.createUser(data);
+  }
+
+  @Get()
+  async getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  async getSingleUser(@Param('id') id: string) {
+    return await this.userService.getSingleUserById(id);
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() data: Partial<User>) {
+    const result = await this.userService.updateuser(id, data);
+    return result;
+  }
+
+  @Patch(':id')
+  async patchUser(@Param('id') id: string, @Body() data: Partial<User>) {
+    const result = await this.userService.patchUser(id, data);
+    return result;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './user.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class UserService {
@@ -10,5 +10,59 @@ export class UserService {
   async createUser(data: Partial<User>): Promise<User> {
     const newUser = new this.userModel(data);
     return await newUser.save();
+  }
+
+  async getUsers() {
+    const result = await this.userModel.find().exec(); //exec used= when promise will be returned
+    return result;
+  }
+
+  async getSingleUserById(id: string) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new NotFoundException('Invalid Object ID');
+    }
+    const user = await this.userModel.findById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async updateuser(id: string, data: Partial<User>) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new NotFoundException('Invalid Object ID');
+    }
+
+    //const updatedUser = await this.userModel.findByIdAndUpdate(id, data, {
+    // new: true,
+    //});
+
+    //recommend for PUT Api//PUT will replace the code completely
+    const updatedUser = await this.userModel.findByIdAndUpdate(
+      id,
+      {
+        name: data.name ?? null,
+        age: data.age ?? null,
+        email: data.email ?? null,
+      },
+      {
+        overwrite: true,
+        new: true,
+      },
+    );
+
+    if (!updatedUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    return updatedUser;
+  }
+
+  async patchUser(id: string, data: Partial<User>) {
+    const updatedUser = await this.userModel.findByIdAndUpdate(id, data, {
+      new: true,
+    });
+
+    return updatedUser;
   }
 }
